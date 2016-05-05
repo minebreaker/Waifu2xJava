3b50f88be90d53260a4c0585a46a528cb2f81216
minebreaker_tf <raptors.f22a.blackwidow.yf23@gmail.com>
2016-05-05 23:14:58 +0900

    進捗保存

--- Waifu2xJava/src/main/java/waifu2j/Waifu2xPyClone.java
+++ Waifu2xJava/src/main/java/waifu2j/Waifu2xPyClone.java
@@ -10,24 +10,27 @@ package waifu2j;
  */
 
 import org.canova.image.loader.ImageLoader;
+import org.nd4j.linalg.api.complex.IComplexNDArray;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.convolution.Convolution;
+import org.nd4j.linalg.convolution.DefaultConvolutionInstance;
 import org.nd4j.linalg.cpu.NDArray;
+import org.nd4j.linalg.cpu.complex.ComplexNDArray;
 import org.nd4j.linalg.factory.Nd4j;
 
 import javax.imageio.ImageIO;
-import javax.json.Json;
-import javax.json.JsonArray;
-import javax.json.JsonReader;
+import javax.json.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.function.Function;
 
 public class Waifu2xPyClone {
 
@@ -48,59 +51,152 @@ public class Waifu2xPyClone {
                 .filter(temp, jImage);
 //        showImage(jImage);
 
-        INDArray image = new ImageLoader().toBgr(jImage);
         // 輝度成分を取り出す
-        INDArray imageYCbCr = convertRgbToYcrcb(image);
-        if (image.length() != imageYCbCr.length()) {
-            throw new Exception("size differs: " + image.length() + " != " + imageYCbCr.length());
+        INDArray image = convertRgbToYcbcr(new ImageLoader().toBgr(jImage));
+        image = regularize(image, val -> val / 255);
+        INDArray planes = new NDArray(new int[] {1, image.slice(0, 1).length(), image.slice(0, 2).length()});
+        planes.putSlice(0, image.slice(0));
+
+        for (JsonValue stepAsValue : model) {
+            JsonObject step = (JsonObject) stepAsValue;
+            System.out.println("input:  " + step.getInt("nInputPlane"));
+            System.out.println("output: " + step.getInt("nOutputPlane"));
+
+            INDArray outputPlanes = Nd4j.zeros(step.getInt("nOutputPlane"), image.slice(0, 1).length(), image.slice(0, 2).length());
+
+            for (int i = 0; i < step.getJsonArray("bias").size(); i++) {
+                INDArray partial = Nd4j.zeros(3, 3); // 固定値でいいはず(kW, kH)
+
+                for (int j = 0; j < step.getJsonArray("weight").size(); i++) {
+                    INDArray ip = planes.slice(0);
+
+                    INDArray kernel = Nd4j.zeros(step.getInt("kW"), step.getInt("kH"));
+                    for (int k = 0; k < step.getInt("kW"); k++) {
+                        for (int l = 0; l < step.getInt("kH"); l++) {
+                            kernel.put(k, l, step.getJsonArray("weight").getJsonArray(i).getJsonArray(j).getJsonArray(k).getJsonNumber(l).doubleValue());
+                        }
+                    }
+//                    System.out.println(ip);
+//                    System.out.println(kernel);
+
+                    // org.nd4j.linalg.api.ops.impl.transforms.convolutionに移行する模様。
+                    // まだim2colとcol2imしかない。convは実装されるのだろうか。
+                    INDArray p = Convolution.convn(ip, kernel, Convolution.Type.FULL);
+
+                    partial.add(p);
+                }
+
+                partial.add(step.getJsonArray("bias").getJsonNumber(i).doubleValue());
+                outputPlanes.putSlice(i, partial);
+            }
+
+            for (int d = 0; d < outputPlanes.slice(0, 0).length(); d++) {
+                for (int h = 0; h < outputPlanes.slice(0, 1).length(); h++) {
+                    for (int w = 0; w < outputPlanes.slice(0, 2).length(); w++) {
+                        if (outputPlanes.getFloat(new int[] {d, h, w}) < 0) {
+                            outputPlanes.muli(0.1);
+                            break;
+                        }
+                    }
+                }
+            }
+            planes = outputPlanes;
         }
 
-//        jImage = ImageLoader.toImage(image); // なんかこいつだと上手く動かない
-        // マトリックスに使うらしいが…
-        new ImageLoader().toBufferedImageRGB(imageYCbCr, jImage);
-
-        showImage(jImage);
+        image = regularize(image, val -> {
+            val = val * 255;
+            if (val > 255)
+                return 255.0;
+            else if (val < 0)
+                return 0.0;
+            else
+                return val;
+        });
+//        INDArray planes = new NDArray(new int[] {1, image.slice(0, 1).length(), image.slice(0, 2).length()});
+        image.putSlice(0, image);
+        showImage(toImage(convertYcbcrToRgb(image), jImage.getType()));
+//        showImage((toImage(toSingleColor(image, 1), jImage.getType())));
+//        showImage(toSingleScale(imageYCbCr, 0));
     }
 
-    private static INDArray convertRgbToYcrcb(INDArray from) {
-//        for (int i = 0 ; i < 4 ; i++) { // 色の種類: 4回 (RGBA)
-//            for (int h = 0 ; h < jImage.getHeight() ; h++) {
-//                for (int w = 0 ; w < jImage.getWidth() ; w++) {
-//                    int[] index = new int[] {i, h, w};
-//                    double r = image.getFloat(new int[] {});
-//                    double g = image.getFloat(new int[] {});
-//                    double b = image.getFloat(new int[] {});
-//                    double value;
-//
-//                    switch (i) {
-//                    case 0:
-//                        image.getFloat(index);
-//                        value = 0.299 * r;
-//                        break;
-//                    default:
-//                        throw new IndexOutOfBoundsException("too many channels");
-//                    }
-//
-//                    value = value / 255;
-//                    imageYCbCr.putScalar(index, value);
-//                }
-//            }
-//        }
+    private static BufferedImage toImage(INDArray from, int ImageType) {
+        int height = from.slice(0, 1).length();
+        int width = from.slice(0, 2).length();
 
-        int originalFacts = from.slice(0, 0).length();
-        int originalHeight = from.slice(0, 1).length();
-        int originalWidth = from.slice(0, 2).length();
+        BufferedImage out = new BufferedImage(width, height, ImageType);
 
-        INDArray out = Nd4j.zeros(originalFacts, originalHeight, originalWidth);
+        INDArray r = from.slice(3);
+        INDArray g = from.slice(2);
+        INDArray b = from.slice(1);
+        INDArray a = from.slice(0);
 
-        INDArray r = from.slice(0);
-        INDArray g = from.slice(1);
-        INDArray b = from.slice(2);
-        for (int h = 0 ; h < 2 ; h++) {
-            for (int w = 0 ; w < 2 ; w++) {
-                float rp =r.getFloat(h, w);
-                float gp =g.getFloat(h, w);
-                float bp =b.getFloat(h, w);
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                WritableRaster raster = out.getRaster();
+                raster.setPixel(w, h, new int[] {r.getInt(h, w), g.getInt(h, w), b.getInt(h, w), a.getInt(h, w)});
+            }
+        }
+        return out;
+    }
+
+    private static INDArray regularize(INDArray from, Function<Double, Double> eval) {
+        INDArray out = Nd4j.zeros(from.slice(0, 0).length(), from.slice(0, 1).length(), from.slice(0, 2).length());
+
+        for (int d = 0; d < from.slice(0, 0).length(); d++) {
+            for (int h = 0; h < from.slice(0, 1).length(); h++) {
+                for (int w = 0; w < from.slice(0, 2).length(); w++) {
+                    out.putScalar(new int[] {d, h, w}, eval.apply(from.getDouble(d, h, w)));
+                }
+            }
+        }
+
+        return out;
+    }
+
+    private static BufferedImage toSingleScale(INDArray from, int channel) {
+        BufferedImage out = new BufferedImage(
+                from.slice(0, 2).length(), from.slice(0, 1).length(), BufferedImage.TYPE_BYTE_GRAY);
+
+        for (int h = 0; h < from.slice(0, 1).length(); h++) {
+            for (int w = 0; w < from.slice(0, 2).length(); w++) {
+                WritableRaster raster = out.getRaster();
+                raster.setPixel(w, h, new int[] {(int) from.getFloat(new int[] {channel, h, w})});
+            }
+        }
+        return out;
+    }
+
+    private static INDArray toSingleColor(INDArray from, int channel) {
+        INDArray out = Nd4j.zeros(from.slice(0, 0).length(), from.slice(0, 1).length(), from.slice(0, 2).length());
+
+        for (int h = 0; h < from.slice(0, 1).length(); h++) {
+            for (int w = 0; w < from.slice(0, 2).length(); w++) {
+                out.putScalar(new int[] {3, h, w}, 0);
+                out.putScalar(new int[] {2, h, w}, 0);
+                out.putScalar(new int[] {1, h, w}, 0);
+                out.putScalar(new int[] {0, h, w}, 255);
+                out.putScalar(new int[] {channel, h, w}, from.getFloat(new int[] {channel, h, w}));
+            }
+        }
+
+        return out;
+    }
+
+    private static INDArray convertRgbToYcbcr(INDArray from) {
+
+        INDArray out = Nd4j.zeros(from.slice(0, 0).length(), from.slice(0, 1).length(), from.slice(0, 2).length());
+
+        INDArray r = from.slice(3);
+        INDArray g = from.slice(2);
+        INDArray b = from.slice(1);
+        INDArray a = from.slice(0);
+
+        for (int h = 0; h < from.slice(0, 1).length(); h++) {
+            for (int w = 0; w < from.slice(0, 2).length(); w++) {
+                float rp = r.getFloat(h, w);
+                float gp = g.getFloat(h, w);
+                float bp = b.getFloat(h, w);
+                float ap = a.getFloat(h, w);
 
                 double y = 0.299 * rp + 0.587 * gp + 0.114 * bp;
                 double cb = -0.169 * rp - 0.331 * gp + 0.500 * bp;
@@ -111,7 +207,39 @@ public class Waifu2xPyClone {
                 out.putScalar(new int[] {0, h, w}, y);
                 out.putScalar(new int[] {1, h, w}, cb);
                 out.putScalar(new int[] {2, h, w}, cr);
-                out.putScalar(new int[] {3, h, w}, from.getFloat(new int[] {3, h, w}));
+                out.putScalar(new int[] {3, h, w}, ap);
+            }
+        }
+
+        return out;
+    }
+
+    private static INDArray convertYcbcrToRgb(INDArray from) {
+
+        INDArray out = Nd4j.zeros(from.slice(0, 0).length(), from.slice(0, 1).length(), from.slice(0, 2).length());
+
+        INDArray y = from.slice(0);
+        INDArray cb = from.slice(1);
+        INDArray cr = from.slice(2);
+        INDArray a = from.slice(3);
+
+        for (int h = 0; h < from.slice(0, 1).length(); h++) {
+            for (int w = 0; w < from.slice(0, 2).length(); w++) {
+                float yp = y.getFloat(h, w);
+                float cbp = cb.getFloat(h, w);
+                float crp = cr.getFloat(h, w);
+                float ap = a.getFloat(h, w);
+
+                double r = yp + 1.402 * crp;
+                double g = yp - 0.344 * cbp - 0.581 * crp;
+                double b = yp + 2.032 * cbp;
+
+//                System.out.println(y + ", " + cb + ", " + cr);
+
+                out.putScalar(new int[] {0, h, w}, r);
+                out.putScalar(new int[] {1, h, w}, g);
+                out.putScalar(new int[] {2, h, w}, b);
+                out.putScalar(new int[] {3, h, w}, ap);
             }
         }
 
@@ -137,7 +265,7 @@ public class Waifu2xPyClone {
             }
         });
 
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.getContentPane().setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
         frame.pack();
         frame.setVisible(true);
--- Waifu2xJava/src/test/java/waifu2j/ND4JSample.java
+++ Waifu2xJava/src/test/java/waifu2j/ND4JSample.java
@@ -1,7 +1,12 @@
 package waifu2j;
 
 import org.junit.Test;
+import org.nd4j.linalg.api.complex.IComplexNDArray;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.convolution.Convolution;
+import org.nd4j.linalg.convolution.DefaultConvolutionInstance;
+import org.nd4j.linalg.cpu.NDArray;
+import org.nd4j.linalg.cpu.complex.ComplexNDArray;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.indexing.NDArrayIndex;
 import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
@@ -33,16 +38,20 @@ public class ND4JSample {
     @Test
     public void testSlicing() {
         INDArray nd = Nd4j.create(new float[] {1, 2, 3, 4, 5, 6, 7, 8}, new int[] {2, 2, 2});
-        // [1, 2
-        //  3, 4]
-        // [5, 6
-        //  7, 8]
+        // [[[1, 2]
+        //   [3, 4]]
+        //  [[5, 6]
+        //   [7, 8]]]
         System.out.println(nd);
         System.out.println(nd.slice(0)); // 1, 2; 3, 4
         System.out.println(nd.slice(1)); // 5, 6; 7, 8
         System.out.println(nd.slice(0, 0)); // 1, 5
         System.out.println(nd.slice(0, 1)); // 1, 3
         System.out.println(nd.slice(0, 2)); // 1, 2
+
+        INDArray nd2 = Nd4j.zeros(1, 2, 2);
+        nd2.putSlice(0, nd.slice(0));
+        System.out.println(nd2);
     }
 
     @Test
@@ -78,7 +87,16 @@ public class ND4JSample {
         }
 
         System.out.println(out);
+    }
 
+    @Test
+    public void convTest() {
+        INDArray nd = Nd4j.create(new float[] {100, 200, 300, 400, 500, 600, 700, 800, 900}, new int[] {3, 3});
+
+        System.out.println(nd);
+        INDArray kernel = Nd4j.create(new double[] {0.5, 0.1, 0.1, 0.1}, new int[] {2, 2});
+
+        System.out.println(Convolution.convn(nd, kernel, Convolution.Type.VALID));
     }
 
 }
